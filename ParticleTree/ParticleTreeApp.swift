1d13c487cf5d2fc5a2b55d619f568d2896422aca
Kate Buyanova <syfdzgxvkk@privaterelay.appleid.com>
2025-12-09 14:53:53 +0100

    Initial Commit

---
+++ ParticleTree/ParticleTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleTreeApp.swift
+//  ParticleTree
+//
+//  Created by Katerina Puntsel on 09/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ParticleTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParticleTreeTests/ParticleTreeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleTreeTests.swift
+//  ParticleTreeTests
+//
+//  Created by Katerina Puntsel on 09/12/2025.
+//
+
+import Testing
+@testable import ParticleTree
+
+struct ParticleTreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
